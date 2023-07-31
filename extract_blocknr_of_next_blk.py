f2c86065eb1494e4a9d0b949b6c3fc0f3f08dac1
Ziyan ZHANG <zhangzy273@mail2.sysu.edu.cn>
2023-07-31 08:04:08 +0800

    增加图片标题

--- extract_blocknr_of_next_blk.py
+++ extract_blocknr_of_next_blk.py
@@ -3,20 +3,27 @@
 Author: Ziyan ZHANG zhangzy273@mail2.sysu.edu.cn
 Date: 2023-07-29 19:45:10
 LastEditors: Ziyan ZHANG zhangzy273@mail2.sysu.edu.cn
-LastEditTime: 2023-07-30 10:52:45
+LastEditTime: 2023-07-30 11:17:50
 FilePath: /draw-block-address/extract_blocknr_of_next_blk.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 import matplotlib.pyplot as plt
-
+import time
+import os
 
 lines = []  # 存储行号
 block_numbers_next_log_block = []  # 存储第一种格式的块号
 block_numbers_metadata_buffer = []  # 存储第一种格式的块号
 jh_b_blocknr = []  # 存储第二种格式的块号
 
-with open('/home/zy/lab/swap_dir/kernel_log.txt', 'r') as file:
+log_file = '/home/zy/lab/swap_dir/kernel_log.txt'
+# 提取log_file的创建时间, 并转化成字符串形式
+log_file_create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getctime(log_file)))
+# 打印log_file的创建时间
+print(f"log_file: {log_file} 的创建时间: {log_file_create_time}")
+
+with open(log_file, 'r') as file:
     for line_number, line in enumerate(file, start=1):
         line = line.rstrip()  # 去除行尾的换行符和空格
         if '我的块号' in line:
@@ -50,6 +57,7 @@ plt.scatter(*zip(*block_numbers_next_log_block), c='r', marker='o', label='next_
 plt.scatter(*zip(*block_numbers_metadata_buffer), c='b', marker='x', label='metadata_buffer')
 plt.scatter(*zip(*jh_b_blocknr), c='g', marker='^', label='jh_b_blocknr')
 plt.legend()
+plt.title(log_file_create_time)
 
 plt.savefig('blocknr.png')
 
